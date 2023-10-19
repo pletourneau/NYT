dd56148299aebf126d6fbb65b2f1db67dc49ed32
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-19 15:24:49 -0700

    2nd

--- MvcApiCall/Controllers/HomeController.cs
+++ MvcApiCall/Controllers/HomeController.cs
@@ -1,22 +1,28 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MvcApiCall.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
 
 namespace MvcApiCall.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
+    private readonly string _apikey;
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _apikey = configuration["NYT"];
     }
 
     public IActionResult Index()
     {
-        return View();
+        Task<string> allArticles = Article.GetArticles(_apikey);
+        return View(allArticles);
     }
+
 
     public IActionResult Privacy()
     {
